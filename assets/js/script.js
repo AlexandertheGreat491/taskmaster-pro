b202196d3e3b2138eea23802a0b2c7b884c71cc4
agvan <agvandyke25@gmail.com>
2022-05-20 11:45:13 -0500

    added events to the sortable for the card

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,7 +46,25 @@ var saveTasks = function() {
 };
 
 $(".card .list-group").sortable({
-connectWith: $(".card .list-group")
+connectWith: $(".card .list-group"),
+scroll: false,
+tolerance: "pointer",
+helper: "clone",
+activate: function(event){
+  console.log("activate", this);
+},
+deactivate: function(event){
+  console.log("deactivate", this);
+},
+over: function(event) {
+  console.log("over", event.target);
+},
+out: function(event){
+  console.log("out", event.target)
+},
+update: function(event) {
+  console.log($(this).children());
+}
 });
 // sortable() turned every element with the class list-group in a sortable list
 /*connectWith is a selector of other sortable elements that the items from this list should be connected to.
