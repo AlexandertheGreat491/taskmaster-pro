35a2df7b82006b69df39ec9bdbbd710e5d03902e
agvan <agvandyke25@gmail.com>
2022-05-21 13:04:16 -0500

    changed the class in sections $("#task-form-modal")

--- assets/js/script.js
+++ assets/js/script.js
@@ -203,7 +203,7 @@ $("modalDueDate").datepicker({
 // The minDate option is the minimum selectable date and the 1 value sets that to be one day from today.
 // The minimum date is now one day from today.
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
